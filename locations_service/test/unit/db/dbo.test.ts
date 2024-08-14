1f7af36607e16d586bab94b21e303b1e1d90698d
TJ <tjbindseil@gmail.com>
2024-08-14 16:36:33 -0600

    that was it, time to clean up

--- locations_service/test/unit/db/dbo.test.ts
+++ locations_service/test/unit/db/dbo.test.ts
@@ -1,139 +1,133 @@
-describe('silly test', () => {
-    it('passes', () => {
-        let x = 1;
-        ++x;
+import { get_app_config } from 'ww-3-app-config-tjb';
+import { Client } from 'ts-postgres';
+import { createPool } from 'generic-pool';
+import { Location } from 'ww-3-models-tjb';
+import {
+    deleteLocation,
+    getLocations,
+    insertLocation,
+} from '../../../src/db/dbo';
+
+// these tests will actually interface with a pg database on my local machine
+// see src/db/index.ts for a command to make the database
+
+type PostedLocation = Omit<Location, 'id'>;
+
+describe('dbo tests', () => {
+    let pgClient: Client;
+    const pool = createPool(
+        {
+            create: async () => {
+                // console.log('TJTAG 1');
+                // ok, so we are in an infinite recursion here
+                // TJTAG 1 keeps printing,
+                // on a whim, I wanted to see if the get_app_config singleton thing was working, and its not
+                // questions:
+                // * is it working in dwf?
+                // * why is there inifinite recursion here?
+                const client = new Client(
+                    get_app_config().locationDbConnectionConfig
+                );
+                console.log('TJTAG 2');
+                await client.connect();
+                console.log('TJTAG 3');
+                client.on('error', console.log);
+                console.log('TJTAG 4');
+                return client;
+            },
+            destroy: async (client: Client) => client.end(),
+            validate: (client: Client) => {
+                return Promise.resolve(!client.closed);
+            },
+        },
+        {
+            testOnBorrow: true,
+            max: 1,
+            min: 1,
+        }
+    );
+
+    const longsPeakLocation: PostedLocation = {
+        name: 'Longs Peak',
+        latitude: 40.255014,
+        longitude: -105.615115,
+    };
+    const crestoneNeedleLocation: PostedLocation = {
+        name: 'Crestone Needle',
+        latitude: 37.964722,
+        longitude: -105.576675,
+    };
+    const mtWhitneyLocation: PostedLocation = {
+        name: 'Mount Whitney',
+        latitude: 36.578581,
+        longitude: -118.291995,
+    };
+    const expectedLocations = [
+        longsPeakLocation,
+        crestoneNeedleLocation,
+        mtWhitneyLocation,
+    ];
+
+    beforeAll(async () => {
+        console.log('beforeAll start');
+        pgClient = await pool.acquire();
+
+        console.log('beforeAll middle');
+
+        const locations = await getLocations(pgClient);
+        if (locations.length > 0) {
+            throw Error('Unit test table is not empty! Abandoning tests');
+        }
+        console.log('beforeAll end');
+    });
+
+    beforeEach(async () => {
+        console.log('beforeEach start');
+        for (let i = 0; i < expectedLocations.length; ++i) {
+            await insertLocation(pgClient, expectedLocations[i]);
+        }
+        console.log('beforeEach end');
+    });
+
+    it('can insert a location', async () => {
+        const initialLocations = await getLocations(pgClient);
+        expect(initialLocations.length).toEqual(expectedLocations.length);
+
+        const grandTetonLocation = {
+            name: 'Grand Teton',
+            latitude: 43.741208,
+            longitude: -110.802414,
+        };
+
+        await insertLocation(pgClient, grandTetonLocation);
+
+        const finalLocations = await getLocations(pgClient);
+        expect(finalLocations.length).toEqual(expectedLocations.length + 1);
+    });
+
+    it('can get all locations', async () => {
+        const initialLocations = await getLocations(pgClient);
+        expect(initialLocations.length).toEqual(expectedLocations.length);
+    });
+
+    it('can delete a location', async () => {
+        const initialLocations = await getLocations(pgClient);
+        expect(initialLocations.length).toEqual(expectedLocations.length);
+
+        await deleteLocation(pgClient, initialLocations[0].id);
+
+        const finalLocations = await getLocations(pgClient);
+        expect(finalLocations.length).toEqual(expectedLocations.length - 1);
+    });
+
+    afterEach(async () => {
+        console.log('afterEach start');
+        await pgClient.query<Location>('TRUNCATE location');
+        console.log('afterEach end');
+    });
+
+    afterAll(async () => {
+        await pool.release(pgClient);
+        pool.drain().then(() => pool.clear());
     });
 });
-// import { get_app_config } from 'ww-3-app-config-tjb';
-// import { Client } from 'ts-postgres';
-// import { createPool } from 'generic-pool';
-// import { Location } from 'ww-3-models-tjb';
-// import {
-//     deleteLocation,
-//     getLocations,
-//     insertLocation,
-// } from '../../../src/db/dbo';
-//
-// // these tests will actually interface with a pg database on my local machine
-// // see src/db/index.ts for a command to make the database
-//
-// type PostedLocation = Omit<Location, 'id'>;
-//
-// describe('dbo tests', () => {
-//     let pgClient: Client;
-//     const pool = createPool(
-//         {
-//             create: async () => {
-//                 // console.log('TJTAG 1');
-//                 // ok, so we are in an infinite recursion here
-//                 // TJTAG 1 keeps printing,
-//                 // on a whim, I wanted to see if the get_app_config singleton thing was working, and its not
-//                 // questions:
-//                 // * is it working in dwf?
-//                 // * why is there inifinite recursion here?
-//                 const client = new Client(
-//                     get_app_config().locationDbConnectionConfig
-//                 );
-//                 console.log('TJTAG 2');
-//                 await client.connect();
-//                 console.log('TJTAG 3');
-//                 client.on('error', console.log);
-//                 console.log('TJTAG 4');
-//                 return client;
-//             },
-//             destroy: async (client: Client) => client.end(),
-//             validate: (client: Client) => {
-//                 return Promise.resolve(!client.closed);
-//             },
-//         },
-//         {
-//             testOnBorrow: true,
-//             max: 1,
-//             min: 1,
-//         }
-//     );
-//
-//     const longsPeakLocation: PostedLocation = {
-//         name: 'Longs Peak',
-//         latitude: 40.255014,
-//         longitude: -105.615115,
-//     };
-//     const crestoneNeedleLocation: PostedLocation = {
-//         name: 'Crestone Needle',
-//         latitude: 37.964722,
-//         longitude: -105.576675,
-//     };
-//     const mtWhitneyLocation: PostedLocation = {
-//         name: 'Mount Whitney',
-//         latitude: 36.578581,
-//         longitude: -118.291995,
-//     };
-//     const expectedLocations = [
-//         longsPeakLocation,
-//         crestoneNeedleLocation,
-//         mtWhitneyLocation,
-//     ];
-//
-//     beforeAll(async () => {
-//         console.log('beforeAll start');
-//         pgClient = await pool.acquire();
-//
-//         console.log('beforeAll middle');
-//
-//         const locations = await getLocations(pgClient);
-//         if (locations.length > 0) {
-//             throw Error('Unit test table is not empty! Abandoning tests');
-//         }
-//         console.log('beforeAll end');
-//     });
-//
-//     beforeEach(async () => {
-//         console.log('beforeEach start');
-//         for (let i = 0; i < expectedLocations.length; ++i) {
-//             await insertLocation(pgClient, expectedLocations[i]);
-//         }
-//         console.log('beforeEach end');
-//     });
-//
-//     it('can insert a location', async () => {
-//         const initialLocations = await getLocations(pgClient);
-//         expect(initialLocations.length).toEqual(expectedLocations.length);
-//
-//         const grandTetonLocation = {
-//             name: 'Grand Teton',
-//             latitude: 43.741208,
-//             longitude: -110.802414,
-//         };
-//
-//         await insertLocation(pgClient, grandTetonLocation);
-//
-//         const finalLocations = await getLocations(pgClient);
-//         expect(finalLocations.length).toEqual(expectedLocations.length + 1);
-//     });
-//
-//     it('can get all locations', async () => {
-//         const initialLocations = await getLocations(pgClient);
-//         expect(initialLocations.length).toEqual(expectedLocations.length);
-//     });
-//
-//     it('can delete a location', async () => {
-//         const initialLocations = await getLocations(pgClient);
-//         expect(initialLocations.length).toEqual(expectedLocations.length);
-//
-//         await deleteLocation(pgClient, initialLocations[0].id);
-//
-//         const finalLocations = await getLocations(pgClient);
-//         expect(finalLocations.length).toEqual(expectedLocations.length - 1);
-//     });
-//
-//     afterEach(async () => {
-//         console.log('afterEach start');
-//         await pgClient.query<Location>('TRUNCATE location');
-//         console.log('afterEach end');
-//     });
-//
-//     afterAll(async () => {
-//         await pool.release(pgClient);
-//         pool.drain().then(() => pool.clear());
-//     });
-// });
