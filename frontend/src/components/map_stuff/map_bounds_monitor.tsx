74f1c12b630aeb2bbffe2c228696bb84ba97b37f
TJ <tjbindseil@gmail.com>
2024-09-29 18:21:17 -0700

    hacky solution doesnt really work

--- frontend/src/components/map_stuff/map_bounds_monitor.tsx
+++ frontend/src/components/map_stuff/map_bounds_monitor.tsx
@@ -7,9 +7,18 @@ export interface MapBoundsMonitorProps {
 
 export const MapBoundsMonitor = ({ setMapBounds }: MapBoundsMonitorProps) => {
   const map = useMap();
+  let lastBounds = map.getBounds();
   map.on('moveend', function () {
     console.log('moveend callback');
-    // setMapBounds(map.getBounds());
+    // TJTAG the below causes this to get called over and over, need to determine why
+    const newBounds = map.getBounds();
+    if (!lastBounds.equals(newBounds)) {
+      lastBounds = newBounds;
+      console.log(
+        `updating bounds, oldBounds=${lastBounds.toBBoxString()} and newBounds=${newBounds.toBBoxString()}`,
+      );
+      setMapBounds(newBounds);
+    }
   });
 
   return <></>;
