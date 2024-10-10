02ba912fa2bb2556907e8a3c8358f85a2829d548
TJ <tjbindseil@gmail.com>
2024-10-09 17:16:03 -0700

    game plan, gonna work on forecast service

--- frontend/src/components/screens/forecast_screen.tsx
+++ frontend/src/components/screens/forecast_screen.tsx
@@ -1,6 +1,18 @@
 import { NavBar } from '../nav_bar';
 
+// take in ids of selected spots
+// request the forecast from forecast service
+// display that shit
+
 export function ForecastScreen() {
+  // it would appear that the dwf app sets the thing (current picture) via the service
+  // it is very reasonable that the user would want to control the caching and reloading of forecasts
+  // this action would have implications outside of the forecast screen
+  //
+  // for this reason, i think it is best that the forecast service controls the fetching and storing of forecast data
+  //
+  // in addition, I would pretty simply be showing things when doing the code for forecast screen, so to the forecast service i will go
+
   return (
     <div className='Home'>
       <NavBar />
