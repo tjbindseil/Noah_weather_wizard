881ac5592a9a04d9eb7d76a00e20e16c66345ae2
TJ <tjbindseil@gmail.com>
2024-10-29 16:08:07 -0600

    clean up a bit

--- frontend/src/services/user_service.tsx
+++ frontend/src/services/user_service.tsx
@@ -13,12 +13,19 @@ import Ajv from 'ajv';
 import { TokenStorageObject } from './utils/token_storage_obj';
 import { useEffect } from 'react';
 
+enum UserSignInStatus {
+  LOGGED_IN,
+  LOGGED_OUT,
+  LOADING,
+}
+
 export interface IUserService {
+  init(): Promise<void>;
   createUser(postUserInput: PostUserInput): Promise<void>;
   authorizeUser(postAuthInput: PostAuthInput): Promise<void>; // TODO rename to sign in
   confirmUser(postConfirmationInput: PostConfirmationInput): Promise<void>;
   deleteUser(): Promise<void>;
-  signedIn(): boolean; // TODO change to logged in
+  getUserSignInStatus(): UserSignInStatus; // TODO change to logged in
   getUsername(): string | undefined;
   logout(): void;
 }
@@ -30,7 +37,8 @@ export const useUserService = (): IUserService =>
 interface UserServiceImpl extends IUserService {
   refreshUser(): Promise<void>;
   username: string | undefined;
-  setUsername: () => Promise<void>;
+  verifyAccessToken: () => Promise<void>;
+  userSignInStatus: UserSignInStatus;
 }
 
 const ajv = new Ajv();
@@ -48,28 +56,9 @@ const UserService = ({ children }: any) => {
 
   const userService = {
     username: undefined,
+    userSignInStatus: UserSignInStatus.LOADING,
 
     async createUser(postUserInput: PostUserInput) {
-      console.log('@@ @@ begin createUser');
-      //
-      // *****
-      // *****
-      // ***** heads up, when running integ tests, everything runs on the laptop, ie where the aws config
-      // ***** is. So, when running on the browser, there is no aws config. this is likely why react isn't working.
-      // *****
-      //
-      // so really, i have two options:
-      // 1. stand up a user service (uhhhh) that just acts as a way to already have the configs set up
-      // 2. figure out a way to bundle the aws config file in the react app
-      //
-      // immediately, i am realizing that, once this react app is being served, it will get weird
-      // while at that point, the user service that I am proposing in option 1 above is going to be
-      // running on a docker container or a lambda or something. i think it will be easy for
-      // that piece of compute to have an IAM role/user/whateva. So, i think option 1 is better (no brainer level better) in the long run
-      //
-      // Lastly, I think that, I can leave the user_facade as is. This means I don't have to rework the integ tests that use it
-      //
-      // *****
       const result = await (
         await fetch(`${baseUrl}/user`, {
           method: 'POST',
@@ -87,7 +76,6 @@ const UserService = ({ children }: any) => {
     },
 
     async authorizeUser(postAuthInput: PostAuthInput) {
-      console.log('@@ @@ begin authorizeUser');
       const result = await (
         await fetch(`${baseUrl}/auth`, {
           method: 'POST',
@@ -104,13 +92,11 @@ const UserService = ({ children }: any) => {
       }
 
       const postAuthOutput = result as unknown as PostAuthOutput;
-      console.log('@@ @@ authorizeUser setting access and refresh token');
       tokenStorageObject.setTokens(postAuthOutput.accessToken, postAuthOutput.refreshToken);
-      await this.setUsername();
+      await this.verifyAccessToken();
     },
 
     async confirmUser(postConfirmationInput: PostConfirmationInput) {
-      console.log('@@ @@ begin confirmUser');
       const result = await (
         await fetch(`${baseUrl}/confirmation`, {
           method: 'POST',
@@ -130,7 +116,6 @@ const UserService = ({ children }: any) => {
     },
 
     async refreshUser() {
-      console.log('@@ @@ begin refreshUser');
       if (tokenStorageObject.getRefreshToken()) {
         const result = await (
           await fetch(`${baseUrl}/refresh`, {
@@ -148,47 +133,25 @@ const UserService = ({ children }: any) => {
         }
 
         const postRefreshOutput = result as unknown as PostRefreshOutput;
-        console.log('@@ @@ refreshUser setting access and refresh tokens');
         tokenStorageObject.setTokens(postRefreshOutput.accessToken, postRefreshOutput.refreshToken);
 
-        await this.setUsername();
+        await this.verifyAccessToken();
       }
     },
 
-    // TODO I think at this point, I am just worried about when a
-    // call is done with an expired accesas token
-    //
-    // options:
-    // 1) get 403, try to refresh token
-    // 2) try to know when a token is in need of a refresh, and schedule it?
-    // 3) try to refresh before any authenticaion of fetch happens
-    //
-    //
-    // I think it would be good to take a step back
-    //
-    // on startup:
-    //   load tokens from cookies
-    //   immediately try to validate tokens
-    //   if successful
-    //     set username
-    //   if immediately usuccessful
-    //     a refresh is attempted
-    //     if successful
-    //       username is set
-    //     else
-    //       user is logged out, cookies are cleared
-
     async init() {
-      if (!tokenStorageObject.loggedIn()) {
+      if (this.userSignInStatus !== UserSignInStatus.LOADING) {
         return;
       }
 
       try {
-        await this.setUsername();
+        await this.verifyAccessToken();
       } catch (e: any) {
         // currently unable to detect what type of errors we are getting ...
         //
         // so assuming an expired token first
+        //
+        // TODO do this better
         try {
           await this.refreshUser();
         } catch (e: any) {
@@ -198,7 +161,6 @@ const UserService = ({ children }: any) => {
     },
 
     async deleteUser() {
-      console.log('@@ @@ begin deleteUser');
       if (tokenStorageObject.getAccessToken()) {
         const result = await (
           await fetch(`${baseUrl}/user`, {
@@ -219,17 +181,16 @@ const UserService = ({ children }: any) => {
       }
     },
 
-    signedIn() {
-      console.log('@@ @@ begin signedIn');
-      return tokenStorageObject.loggedIn();
+    getUserSignInStatus() {
+      this.userSignInStatus;
     },
 
-    async setUsername() {
-      console.log('@@ @@ begin setUsername');
+    async verifyAccessToken() {
       const accessToken = tokenStorageObject.getAccessToken();
       if (accessToken) {
         const decoded = await defaultVerifier.verify(accessToken);
         this.username = decoded.username;
+        this.userSignInStatus = UserSignInStatus.LOGGED_IN;
       }
     },
 
@@ -238,18 +199,14 @@ const UserService = ({ children }: any) => {
     },
 
     logout() {
-      console.log('@@ @@ begin logout');
       this.username = undefined;
-      console.log('@@ @@ logout setting access and refresh token to undefined');
-      tokenStorageObject.signOut();
+      tokenStorageObject.clearTokens();
+      this.userSignInStatus = UserSignInStatus.LOGGED_OUT;
     },
   } as UserServiceImpl;
 
   useEffect(() => {
-    console.log('@@ @@ user_service useEffect');
-    if (tokenStorageObject.loggedIn()) {
-      userService.setUsername();
-    }
+    userService.init();
   });
 
   return (
--- frontend/src/services/utils/token_storage_obj.ts
+++ frontend/src/services/utils/token_storage_obj.ts
@@ -11,15 +11,6 @@ export class TokenStorageObject {
     // attempt to load the cookies right off the bat, if they're not there, assume nobody is logged in
     this.accessToken = Cookies.get(ACCESS_TOKEN_KEY);
     this.refreshToken = Cookies.get(REFRESH_TOKEN_KEY);
-
-    // TODO
-    // we need to set the username here
-    // but,
-    // that is an asynchronous operation
-    //
-    // so, I need to either:
-    // 1. bite the bullet and handle loading
-    // 2. find a way to do it during start up
   }
 
   // TODO determine expiration time
@@ -41,12 +32,7 @@ export class TokenStorageObject {
     return this.refreshToken;
   }
 
-  // TODO this is not really accurate, could have issues refreshing
-  public loggedIn() {
-    return this.accessToken !== undefined && this.refreshToken !== undefined;
-  }
-
-  public signOut() {
+  public clearTokens() {
     this.accessToken = undefined;
     this.refreshToken = undefined;
     Cookies.remove(ACCESS_TOKEN_KEY);
