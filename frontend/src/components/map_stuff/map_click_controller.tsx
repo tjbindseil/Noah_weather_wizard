903ada96a1dcd2604c0fe43fb4cdb91c6f2488e7
TJ <tjbindseil@gmail.com>
2024-11-01 22:58:05 -0600

    get map click controller back

---
+++ frontend/src/components/map_stuff/map_click_controller.tsx
@@ -0,0 +1,62 @@
+import { LeafletEventHandlerFn } from 'leaflet';
+import { useState } from 'react';
+import { Popup, Marker, useMapEvent } from 'react-leaflet';
+import { LeafletMarkerColorOptions, makeColoredIcon } from './marker_color';
+import { SelectedSpotProps } from './selected_spot';
+
+export interface MapClickControllerProps {
+  saveSelectedSpot: (selectedSpot: SelectedSpotProps) => Promise<void>;
+  color: LeafletMarkerColorOptions;
+}
+
+export function MapClickController({ saveSelectedSpot, color }: MapClickControllerProps) {
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [popupLat, setPopupLat] = useState(0);
+  const [popupLong, setPopupLong] = useState(0);
+  const [popupName, setPopupName] = useState('');
+
+  useMapEvent('click', (e) => {
+    setPopupOpen(true);
+    setPopupLat(e.latlng.lat);
+    setPopupLong(e.latlng.lng);
+  });
+
+  const openPopup: LeafletEventHandlerFn = (e) => {
+    e.target.openPopup();
+  };
+
+  const coloredIcon = makeColoredIcon(color);
+
+  // TODO remove the popup marker
+  return popupOpen ? (
+    <Marker position={[popupLat, popupLong]} icon={coloredIcon} eventHandlers={{ add: openPopup }}>
+      <Popup>
+        <label htmlFor='popupName'>Name:</label>
+        <input
+          type='text'
+          id='popupName'
+          value={popupName}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setPopupName(event.target.value);
+          }}
+        />
+        <button
+          onClick={async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+            await saveSelectedSpot({
+              latitude: popupLat,
+              longitude: popupLong,
+              name: popupName,
+              color,
+            });
+            setPopupOpen(false);
+            event.stopPropagation();
+          }}
+        >
+          Save Spot
+        </button>
+      </Popup>
+    </Marker>
+  ) : (
+    <></>
+  );
+}
