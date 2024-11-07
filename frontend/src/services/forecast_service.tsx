0a6217a6cf48bab1dd76176f394cfc671301aba0
TJ <tjbindseil@gmail.com>
2024-11-06 21:59:16 -0700

    got it done for forecasts as well

--- frontend/src/services/forecast_service.tsx
+++ frontend/src/services/forecast_service.tsx
@@ -1,7 +1,7 @@
 import { _schema, GetForecastsInput, GetForecastsOutput } from 'ww-3-models-tjb';
 import Contextualizer from './contextualizer';
 import ProvidedServices from './provided_services';
-import Ajv from 'ajv';
+import { getWithError } from './fetch_wrapper';
 
 export interface IForecastService {
   getForecasts(input: GetForecastsInput): Promise<GetForecastsOutput>;
@@ -13,30 +13,16 @@ export const ForecastServiceContext = Contextualizer.createContext(
 export const useForecastService = (): IForecastService =>
   Contextualizer.use<IForecastService>(ProvidedServices.ForecastService);
 
-// TODO check this out
-// might want to use a similar pattern to the backend
-// so service object that utilizes virtuality to delegate owning the validator to child objects, probably overkill tho
-
-const ajv = new Ajv();
-
 const ForecastService = ({ children }: any) => {
   const baseUrl = 'http://localhost:8081';
-  const getForecastsOutputValidator = ajv.compile(_schema.GetForecastsOutput);
 
   const forecastService = {
     async getForecasts(input: GetForecastsInput): Promise<GetForecastsOutput> {
-      const result = await (
-        await fetch(`${baseUrl}/forecasts?` + new URLSearchParams({ ...input }), {
-          method: 'GET',
-          mode: 'cors',
-        })
-      ).json();
-
-      if (!getForecastsOutputValidator(result)) {
-        throw new Error(`invalid response: ${result}`);
-      }
-
-      return result as GetForecastsOutput;
+      return await getWithError<GetForecastsOutput>(
+        { ...input },
+        `${baseUrl}/forecasts`,
+        _schema.GetForecastsOutput,
+      );
     },
   };
 
