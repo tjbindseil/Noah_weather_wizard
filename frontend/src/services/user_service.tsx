428473fbc72c4fd76e10eedbb63ea17948dfa7de
TJ <tjbindseil@gmail.com>
2024-11-06 22:14:37 -0700

    use it in user service

--- frontend/src/services/user_service.tsx
+++ frontend/src/services/user_service.tsx
@@ -1,19 +1,24 @@
 import {
+  DeleteUserInput,
+  DeleteUserOutput,
   PostAuthInput,
   PostAuthOutput,
   PostConfirmationInput,
+  PostConfirmationOutput,
   PostNewConfirmationCodeInput,
   PostNewConfirmationCodeOutput,
+  PostRefreshInput,
   PostRefreshOutput,
   PostUserInput,
+  PostUserOutput,
   _schema,
 } from 'ww-3-models-tjb';
 import Contextualizer from './contextualizer';
 import ProvidedServices from './provided_services';
 import { defaultVerifier } from 'ww-3-api-tjb';
-import Ajv from 'ajv';
 import { TokenStorageObject } from './utils/token_storage_obj';
 import { useEffect } from 'react';
+import { fetchWithError, HTTPMethod } from './fetch_wrapper';
 
 export enum UserSignInStatus {
   LOGGED_IN,
@@ -23,11 +28,11 @@ export enum UserSignInStatus {
 
 export interface IUserService {
   init(): Promise<void>;
-  createUser(postUserInput: PostUserInput): Promise<void>;
-  authorizeUser(postAuthInput: PostAuthInput): Promise<void>; // TODO rename to sign in
-  confirmUser(postConfirmationInput: PostConfirmationInput): Promise<void>;
+  createUser(input: PostUserInput): Promise<PostUserOutput>;
+  authorizeUser(input: PostAuthInput): Promise<void>;
+  confirmUser(input: PostConfirmationInput): Promise<void>;
   deleteUser(): Promise<void>;
-  getUserSignInStatus(): UserSignInStatus; // TODO change to logged in
+  getUserSignInStatus(): UserSignInStatus;
   getUsername(): string | undefined;
   logout(): void;
   getAccessToken(): Promise<string>;
@@ -47,17 +52,9 @@ interface UserServiceImpl extends IUserService {
   userSignInStatus: UserSignInStatus;
 }
 
-const ajv = new Ajv();
-
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 const UserService = ({ children }: any) => {
   const baseUrl = 'http://localhost:8082';
-
-  const postUserOutputValidator = ajv.compile(_schema.PostUserOutput);
-  const postAuthOutputValidator = ajv.compile(_schema.PostAuthOutput);
-  const postConfirmationOutputValidator = ajv.compile(_schema.PostConfirmationOutput);
-  const postRefreshOutputValidator = ajv.compile(_schema.PostRefreshOutput);
-  const deleteUserOutputValidator = ajv.compile(_schema.DeleteUserOutput);
 
   const tokenStorageObject = new TokenStorageObject();
 
@@ -65,83 +62,48 @@ const UserService = ({ children }: any) => {
     username: undefined,
     userSignInStatus: UserSignInStatus.LOADING,
 
-    async createUser(postUserInput: PostUserInput) {
-      const result = await (
-        await fetch(`${baseUrl}/user`, {
-          method: 'POST',
-          mode: 'cors',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            ...postUserInput,
-          }),
-        })
-      ).json();
-
-      if (!postUserOutputValidator(result)) {
-        throw new Error(`UserService::postUser - invalid response: ${JSON.stringify(result)}`);
-      }
+    async createUser(input: PostUserInput) {
+      return await fetchWithError<PostUserInput, PostUserOutput>(
+        input,
+        `${baseUrl}/user`,
+        HTTPMethod.POST,
+        _schema.PostUserOutput,
+      );
     },
 
-    async authorizeUser(postAuthInput: PostAuthInput) {
-      const result = await (
-        await fetch(`${baseUrl}/auth`, {
-          method: 'POST',
-          mode: 'cors',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            ...postAuthInput,
-          }),
-        })
-      ).json();
-
-      if (!postAuthOutputValidator(result)) {
-        throw new Error(`UserService::postAuth - invalid response: ${JSON.stringify(result)}`);
-      }
-
-      const postAuthOutput = result as unknown as PostAuthOutput;
+    async authorizeUser(input: PostAuthInput) {
+      const postAuthOutput = await fetchWithError<PostAuthInput, PostAuthOutput>(
+        input,
+        `${baseUrl}/auth`,
+        HTTPMethod.POST,
+        _schema.PostAuthOutput,
+      );
       tokenStorageObject.setTokens(postAuthOutput.accessToken, postAuthOutput.refreshToken);
       await this.verifyAccessToken();
     },
 
-    async confirmUser(postConfirmationInput: PostConfirmationInput) {
-      const result = await (
-        await fetch(`${baseUrl}/confirmation`, {
-          method: 'POST',
-          mode: 'cors',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            ...postConfirmationInput,
-          }),
-        })
-      ).json();
-
-      if (!postConfirmationOutputValidator(result)) {
-        throw new Error(
-          `UserService::postConfirmation - invalid response: ${JSON.stringify(result)}`,
-        );
-      }
+    async confirmUser(input: PostConfirmationInput) {
+      await fetchWithError<PostConfirmationInput, PostConfirmationOutput>(
+        input,
+        `${baseUrl}/confirmation`,
+        HTTPMethod.POST,
+        _schema.PostConfirmationOutput,
+      );
     },
 
     async refreshUser() {
-      if (tokenStorageObject.getRefreshToken()) {
-        const result = await (
-          await fetch(`${baseUrl}/refresh`, {
-            method: 'POST',
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              refreshToken: tokenStorageObject.getRefreshToken(),
-            }),
-          })
-        ).json();
+      const refreshToken = tokenStorageObject.getRefreshToken();
+      if (refreshToken) {
+        const postRefreshOutput = await fetchWithError<PostRefreshInput, PostRefreshOutput>(
+          {
+            refreshToken,
+          },
+          `${baseUrl}/refresh`,
+          HTTPMethod.POST,
+          _schema.PostRefreshOutput,
+        );
 
-        if (!postRefreshOutputValidator(result)) {
-          throw new Error(`UserService::postRefresh - invalid response: ${JSON.stringify(result)}`);
-        }
-
-        const postRefreshOutput = result as unknown as PostRefreshOutput;
         tokenStorageObject.setTokens(postRefreshOutput.accessToken, postRefreshOutput.refreshToken);
-
         await this.verifyAccessToken();
       }
     },
@@ -168,24 +130,19 @@ const UserService = ({ children }: any) => {
     },
 
     async deleteUser() {
-      if (tokenStorageObject.getAccessToken()) {
-        const result = await (
-          await fetch(`${baseUrl}/user`, {
-            method: 'DELETE',
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              accessToken: tokenStorageObject.getAccessToken(),
-            }),
-          })
-        ).json();
-
-        if (!deleteUserOutputValidator(result)) {
-          throw new Error(`UserService::deleteUser - invalid response: ${JSON.stringify(result)}`);
-        }
-
-        this.logout();
+      const accessToken = tokenStorageObject.getAccessToken();
+      if (accessToken) {
+        await fetchWithError<DeleteUserInput, DeleteUserOutput>(
+          {
+            accessToken,
+          },
+          `${baseUrl}/user`,
+          HTTPMethod.DELETE,
+          _schema.DeleteUserOutput,
+        );
       }
+
+      this.logout();
     },
 
     getUserSignInStatus() {
