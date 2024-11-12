88e09e7d8173499c1e4c2fccbbd91ef45aea601a
TJ <tjbindseil@gmail.com>
2024-11-12 09:11:02 -0700

    add timestamp to logs

--- pm2_ecosystem_file.js
+++ pm2_ecosystem_file.js
@@ -3,14 +3,17 @@ module.exports = {
     {
       name: "spot_service",
       script: "spot_service/build/src/index.js",
+      time: true,
     },
     {
       name: "forecast_service",
       script: "forecast_service/build/src/index.js",
+      time: true,
     },
     {
       name: "user_service",
       script: "user_service/build/src/index.js",
+      time: true,
     },
   ],
 };
