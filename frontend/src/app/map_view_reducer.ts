7daab2c74717828133a059b278033e6cda7bcd79
TJ <tjbindseil@gmail.com>
2024-11-23 16:11:54 -0700

    save center and zoom in cookies

--- frontend/src/app/map_view_reducer.ts
+++ frontend/src/app/map_view_reducer.ts
@@ -1,23 +1,49 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import Cookies from 'js-cookie';
+
+const CENTER_LAT_KEY = 'CENTER_LAT_KEY';
+const CENTER_LNG_KEY = 'CENTER_LNG_KEY';
+const ZOOM_KEY = 'ZOOM_KEY';
+
+const getInitialStateFromCookies = () => {
+  const lat = isNaN(Number(Cookies.get(CENTER_LAT_KEY)))
+    ? 40.255014
+    : Number(Cookies.get(CENTER_LAT_KEY));
+  const lng = isNaN(Number(Cookies.get(CENTER_LNG_KEY)))
+    ? -105.615115
+    : Number(Cookies.get(CENTER_LNG_KEY));
+  const zoom = isNaN(Number(Cookies.get(ZOOM_KEY))) ? 13 : Number(Cookies.get(ZOOM_KEY));
+
+  return {
+    zoom,
+    center: { lat, lng },
+    desiredCenter: { lat, lng },
+    toggleToCenter: false,
+    mapBounds: { sw: { lat: 0, lng: 0 }, ne: { lat: 0, lng: 0 } },
+  };
+};
 
 export const mapViewSlice = createSlice({
   name: 'map_view',
-  initialState: {
-    zoom: 13,
-    center: { lat: 40.255014, lng: -105.615115 },
-    desiredCenter: { lat: 40.255014, lng: -105.615115 },
-    toggleToCenter: false,
-    mapBounds: { sw: { lat: 0, lng: 0 }, ne: { lat: 0, lng: 0 } },
-  },
+  initialState: getInitialStateFromCookies(),
   reducers: {
     setZoom: (state, action: PayloadAction<number>) => {
-      state.zoom = action.payload;
+      // save to cookies as well
+      const newZoom = action.payload;
+      Cookies.set(ZOOM_KEY, newZoom.toString());
+
+      state.zoom = newZoom;
     },
     setCenter: (state, action: PayloadAction<{ lat: number; lng: number }>) => {
-      state.center = action.payload;
+      const newCenter = action.payload;
+
+      Cookies.set(CENTER_LAT_KEY, newCenter.lat.toString());
+      Cookies.set(CENTER_LNG_KEY, newCenter.lng.toString());
+
+      state.center = newCenter;
 
       // we set this here because otherwise its hard to implement the center button in the latlng input thing
-      state.desiredCenter = action.payload;
+      state.desiredCenter = newCenter;
     },
     setDesiredCenter: (state, action: PayloadAction<{ lat: number; lng: number }>) => {
       state.desiredCenter = action.payload;
--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -4,17 +4,8 @@ import visibleSpotsReducer from './visible_spots_reducer';
 
 export const store = configureStore({
   reducer: {
-    // includes information on what is favorited, hovered, and selected, or maybe user status has favorites?
-    // not sure if we want to track all favorites or which of the shown spots are favorited
-    //
-    // hmm, i guerss all spots could have a forecasts
     visibleSpots: visibleSpotsReducer,
-    //     userStatus: userStatusReducer, // includes login status and optional name
-    mapView: mapViewReducer, // TODO store in cookies
-
-    // I think the below are too narrowly scoped to be of use here
-    // latLngInput: latLngInputReducer,
-    // forecastDropDown: forecastDropDownReducer,
+    mapView: mapViewReducer,
   },
 });
 
