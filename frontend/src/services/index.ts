c59b5870e6a0b8773ff48b6294c978b65fae4ffa
TJ <tjbindseil@gmail.com>
2024-11-25 19:23:45 -0700

    minorthing

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
 import { get_app_config } from 'ww-3-app-config-tjb';
 
-export const baseUrl = `http://${get_app_config(process.env.REACT_APP_WW_ENV).frontendServiceHost}:${get_app_config().frontendServicePort}`;
+// TODO could i use a global.d.ts file to augment the get_app_config and automatically feed it the REACT_APP_WW_ENV var?
+export const baseUrl = `http://${get_app_config(process.env.REACT_APP_WW_ENV).frontendServiceHost}:${get_app_config(process.env.REACT_APP_WW_ENV).frontendServicePort}`;
