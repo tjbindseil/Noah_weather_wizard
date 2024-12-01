2fcec89581c34551ca1ff8fbda7e7bb355a4ee43
TJ <tjbindseil@gmail.com>
2024-12-01 09:00:33 -0700

    route frontend pages to home

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 npm set maxsockets 1
-export NODE_OPTIONS="--max-old-space-size=512"
+export NODE_OPTIONS="--max-old-space-size=2048"
 
 declare -a to_build=("models" "app_config" "api" "utilities" "user_facade" "forecast_fetcher" "forecast_service" "spot_service" "user_service")
 for dir in "${to_build[@]}"
